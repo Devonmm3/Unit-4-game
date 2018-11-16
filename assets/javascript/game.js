2c898d5f6cc304a0b9aa7332889281058146ea7f
DevonMM <Devon.m.moran@gmail.com>
2018-11-15 20:23:39 -0500

    fourth

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,35 @@
-    var random = Math.floor(Math.random() * 120) + 19;
+$(document).ready(function () {
+    console.log("go");
 
-    var valueCrystal1 = Math.floor(Math.random() * 12) + 1;
-    var valueCrystal2 = Math.floor(Math.random() * 12) + 1;
-    var valueCrystal3 = Math.floor(Math.random() * 12) + 1;
-    var valueCrystal4 = Math.floor(Math.random() * 12) + 1;
 
+    var valueCrystal1 = Math.floor(Math.random() * 12 + 1);
+    console.log(valueCrystal1);
+    var valueCrystal2 = Math.floor(Math.random() * 12 + 1);
+    console.log(valueCrystal2);
+    var valueCrystal3 = Math.floor(Math.random() * 12 + 1);
+    console.log(valueCrystal3);
+    var valueCrystal4 = Math.floor(Math.random() * 12 + 1);
+    console.log(valueCrystal4);
     var guessTotal = 0;
     var wins = 0;
     var losses = 0;
+    var gameNumber = Math.floor(Math.random() * 102 + 19);
+    console.log(gameNumber);
+    $("#computersNumber").text(gameNumber);
+    $("#numberWins").text(wins);
+    $("#numberLosses").text(losses);
 
-    $(document).ready(function () {
-                $("#computersNumber").text(random);
-                $("#numberWins").text(wins);
-                $("#numberLosses").text(losses);
 
-                function reset() {
-                    random = Math.floor(Math.random() * 120) + 19;
-                    console.log(random);
-                    $("computersNumber").text(random);
-
-                }
+    $("#valueCrystal1").on("click", function () {
+        console.log(valueCrystal1);
+    });
+    $("#valueCrystal2").on("click", function () {
+        console.log(valueCrystal2);
+    });
+    $("#valueCrystal").on("click", function () {
+        console.log(valueCrystal3);
+    });
+    $("#valueCrystal").on("click", function () {
+        console.log(valueCrystal4);
+    });
+});
